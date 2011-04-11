ae807df628495305c392330275688b7f3a052886
Philippe Blayo <philippe.blayo@gmail.com>
2011-04-11 11:13:44 +0200

    Un exercice de plus

---
+++ chapter-2/src/scjp/chapter/two/pblayo/PhilippeOne.java
@@ -0,0 +1,29 @@
+package scjp.two.pblayo;
+
+public class PhilippeOne {
+
+	short methodOne() {
+		int result = 0;
+		return result;  // line 7
+	}
+
+	int methodTwo() {
+		short result = 0;
+		return result;  // line 12
+	}
+
+	public static void main(String[] args) {
+		PhilippeOne instance = new PhilippeOne();
+		System.out.println(instance.methodOne());  // line 17
+		System.out.println(instance.methodTwo());  // line 18
+	}
+}
+
+/* Question :
+ * What is the result of compilation :
+ * A/ 00
+ * B/ compilation error at line 7
+ * C/ compilation error at line 12
+ * D/ compilation error at line 17
+ * E/ compilation error at line 18
+ */
