53bf87e642ed669a65184d306ca18ed42afe4bb4
Mehdi BEN HAJ ABBES <Mehdi.Benhajabbes@direct-energie.com>
2011-04-19 14:21:05 +0200

    my exercises + a maven project

---
+++ ch2/src/main/java/scjp/chapter2/peiniau/QuestionOne.java
@@ -0,0 +1,12 @@
+package scjp.chapter2.peiniau;
+
+public class QuestionOne {
+	
+	public void example() {
+		System.out.println("This is an example");
+	}
+
+	public static void main(String... args) {
+		new QuestionOne().example();
+	}
+}
