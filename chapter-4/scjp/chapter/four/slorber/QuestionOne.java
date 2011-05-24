7d2c1f7ae49925207539ddd2acf26cb30878e8a9
Sebastien Lorber <lorber.sebastien@gmail.com>
2011-05-24 12:32:28 +0200

    refactor

---
+++ chapter-4/scjp/chapter/four/slorber/QuestionOne.java
@@ -0,0 +1,13 @@
+package scjp.chapter.four.slorber;
+
+
+public class QuestionOne {
+	
+	public void example() {
+		System.out.println("This is an example");
+	}
+
+	public static void main(String... args) {
+		new QuestionOne().example();
+	}
+}
